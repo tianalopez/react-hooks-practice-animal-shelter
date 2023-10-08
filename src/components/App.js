5c92ff90756fac41cadd1aeacaf5512ec20884c4
Tiana Lopez <tianadavin@gmail.com>
2023-10-08 16:33:00 -0700

    completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,70 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  const [adoptedPet, setAdoptedPet] = useState(false)
+
+  //change selection for filter
+  const onChangeType = (selectionValue) => {
+    setFilters((filter) => selectionValue)
+  }
+  //find pets clicked
+  const onFindPetsClick = () => {
+    let URL = "";
+    if (filters === "micropig") {
+      URL = "http://localhost:3001/pets?type=micropig"
+    }
+    else if (filters === 'cat') {
+      URL = "http://localhost:3001/pets?type=cat"
+    }
+    else if (filters === "dog") {
+      URL = "http://localhost:3001/pets?type=dog"
+    }
+    else {
+      URL = "http://localhost:3001/pets"
+    }
+
+    fetch(URL)
+    .then(r=> r.json())
+    .then(setPets)
+  }
+
+  const onAdoptPet = (id) => {
+    // fetch(`http://localhost:3001/pets/${id}`, {
+    //   method: "PATCH",
+    //   headers: {
+    //     "Content-Type" : "application/json",
+    //     "Accept": "application/json"
+    //   },
+    //   body: JSON.stringify({isAdopted: true})
+    // })
+    // .then(r => r.json())
+    // .then((petObj) => {
+    //   const newArray = pets.map((pet) => {
+    //     if (pet.id === petObj.id) {
+    //       console.log("match")
+    //       return petObj
+    //     }
+    //     else {
+    //       console.log("no match")
+    //       return pet
+    //     }
+    //   })
+    //   setPets((pets) => newArray)
+    // })
+
+
+    const newArray = pets.map((pet) => {
+      if (pet.id === id) {
+        pet.isAdopted = true
+        return pet
+      }
+      else {
+        return pet
+      }
+  })
+    console.log(newArray)
+    setPets((pets) => newArray)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +79,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+
+function PetBrowser({pets, onAdoptPet}) {
+  const renderPet = pets.map((pet) => (
+    <Pet key={pet.id} onAdoptPet={onAdoptPet} pet={pet}/>
+  ))
+  return <div className="ui cards">{renderPet}</div>;
 }
 
 export default PetBrowser;
